973a1b9458cf7df0ef193bccd4ce4274a6932250
roman <j-kio@mail.ru>
2022-12-11 14:37:54 +0500

    lab3 update example8

--- src/lab3/number8/example8.java
+++ src/lab3/number8/example8.java
@@ -4,17 +4,15 @@ public class example8 {
     public static void main(String[] args) {
         int size = 10;
         char[] charArray = new char[size];
-        char[] arrGlas = new char[] {'A', 'E', 'I', 'O', 'U', 'Y'};
+        char[] arrWord = new char[] {'A', 'E', 'I', 'O', 'U', 'Y'};
         char firstChar = 'A';
 
 
         for(int i = 0; i < size; i++)
         {
-            Boolean flag = true;
-            for(int j = 0; j < arrGlas.length; j++)
-            {
-                if(firstChar == arrGlas[j])
-                {
+            boolean flag = true;
+            for (char arrGla : arrWord) {
+                if (firstChar == arrGla) {
                     i--;
                     flag = false;
                     break;
